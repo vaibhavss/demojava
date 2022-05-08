e570618fa9c0d13c1c9d6c11f8092381bb6809ff
Vaibhav Sarmalkar <maillvaibhav@gmail.com>
2022-05-08 15:29:36 +0100

    Formatted the code with online formatter.

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,18 +9,18 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
 public class Main {
-public static void main(String[] args) throws IOException{
-JobConf conf = new JobConf(Word_count_Reducer.class);
-conf.setJobName("WordCount");
-conf.setOutputKeyClass(Text.class);
-conf.setOutputValueClass(IntWritable.class);
-conf.setMapperClass(Word_Count_Mapper.class);
-conf.setCombinerClass(Word_count_Reducer.class);
-conf.setReducerClass(Word_count_Reducer.class);   
-conf.setInputFormat(TextInputFormat.class);
-conf.setOutputFormat(TextOutputFormat.class);   
-FileInputFormat.setInputPaths(conf,new Path(args[0]));
-FileOutputFormat.setOutputPath(conf,new Path(args[1]));   
-JobClient.runJob(conf);
-}
+  public static void main(String[] args) throws IOException {
+    JobConf conf = new JobConf(Word_count_Reducer.class);
+    conf.setJobName("WordCount");
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+    conf.setMapperClass(Word_Count_Mapper.class);
+    conf.setCombinerClass(Word_count_Reducer.class);
+    conf.setReducerClass(Word_count_Reducer.class);
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    JobClient.runJob(conf);
+  }
 }
--- Word_count_Reducer.java
+++ Word_count_Reducer.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.util.Iterator;
 import org.apache.hadoop.io.IntWritable;
@@ -8,13 +7,13 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 
-public class Word_count_Reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable> {
-public void reduce(Text key, Iterator<IntWritable> values,OutputCollector<Text,IntWritable> output,
-Reporter reporter) throws IOException {
-int sum=0;
-while (values.hasNext()) {
-sum+=values.next().get();
-}
-output.collect(key,new IntWritable(sum));
-}
+public class Word_count_Reducer extends MapReduceBase implements Reducer < Text, IntWritable, Text, IntWritable > {
+  public void reduce(Text key, Iterator < IntWritable > values, OutputCollector < Text, IntWritable > output,
+    Reporter reporter) throws IOException {
+    int sum = 0;
+    while (values.hasNext()) {
+      sum += values.next().get();
+    }
+    output.collect(key, new IntWritable(sum));
+  }
 }
