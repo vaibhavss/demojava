6242f5e7cae2e0a2d52bcecad92749cfc89c7595
Vaibhav Sarmalkar <maillvaibhav@gmail.com>
2022-05-08 15:17:44 +0100

    Added new files to the repo for aws demo.

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+public class Main {
+public static void main(String[] args) throws IOException{
+JobConf conf = new JobConf(Word_count_Reducer.class);
+conf.setJobName("WordCount");
+conf.setOutputKeyClass(Text.class);
+conf.setOutputValueClass(IntWritable.class);
+conf.setMapperClass(Word_Count_Mapper.class);
+conf.setCombinerClass(Word_count_Reducer.class);
+conf.setReducerClass(Word_count_Reducer.class);   
+conf.setInputFormat(TextInputFormat.class);
+conf.setOutputFormat(TextOutputFormat.class);   
+FileInputFormat.setInputPaths(conf,new Path(args[0]));
+FileOutputFormat.setOutputPath(conf,new Path(args[1]));   
+JobClient.runJob(conf);
+}
+}
